b5b97c4d1bbde712982b787e41177c6ccd538224
yolypham <yolypham@gmail.com>
2018-12-16 17:10:26 -0500

    heroku db

--- api/utils/constants.js
+++ api/utils/constants.js
@@ -3,7 +3,11 @@
 exports.PORT = process.env.PORT || 3001;
 exports.SECRET = process.env.SECRET || 'super-secret-passphrase';
 
-exports.MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/keto-recipes";
+// exports.MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/keto-recipes";
+
+
+exports.MONGODB_URI = process.env.MONGODB_URI || "mongodb://ypham:y123456@ds135714.mlab.com:35714/heroku_r06w3v3g";
+
 
 
 
